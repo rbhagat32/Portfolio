b163a33c3ac85cbe619fea27525d40a73d7bafdf
rbhagat32 <raghavbhagat32@gmail.com>
2024-04-19 10:58:13 +0530

    light mode particles fix

--- scripts/dark-mode.js
+++ scripts/dark-mode.js
@@ -1,7 +1,9 @@
 const darkMode = document.querySelectorAll('#dark-mode');
+const particles = document.querySelector('#particles-js');
 
 darkMode.forEach((button) => {
     button.addEventListener('click', () => {
         document.body.classList.toggle('light-mode');
+        particles.classList.toggle('invert');
     });
 });
