a23fe42dc653b018d11ba2f02f583ed16973d435
rbhagat32 <raghavbhagat32@gmail.com>
2024-05-11 00:47:47 +0530

    added local storage for theme

--- scripts/dark-mode.js
+++ scripts/dark-mode.js
@@ -5,5 +5,16 @@ darkMode.forEach((button) => {
     button.addEventListener('click', () => {
         document.body.classList.toggle('light-mode');
         particles.classList.toggle('invert');
+        
+        localStorage.setItem('theme', document.body.classList.contains('light-mode') ? 'light' : 'dark');
     });
 });
+
+if (localStorage.getItem('theme') === 'light') {
+    document.body.classList.add('light-mode');
+    particles.classList.add('invert');
+}
+else if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.remove('light-mode');
+    particles.classList.remove('invert');
+}
